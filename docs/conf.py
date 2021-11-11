cad8d601c675ecc51c6189f81db6f787ec79b647
xinetzone <xinzone@outlook.com>
2021-11-11 08:33:28 +0800

    new file:   docs/favicon.png

--- docs/conf.py
+++ docs/conf.py
@@ -231,7 +231,7 @@ html_logo = 'logo.jpg'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "page-logo.jfif"
+html_favicon = "favicon.png"
 
 # -- 自定义网站的标题 --------------
 # html_title = '动手学习 Python'
